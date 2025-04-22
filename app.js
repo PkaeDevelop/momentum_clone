548631192e5834c1eba0cac2d548c75cf7ee9dc2
PkaeDevelop <rkdcksgh104@gmail.com>
2025-04-22 18:34:27 +0900

    Update app.js
    노마드 코더 2.4까지 강의 내용 정리

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hi");
+//const == 값이 바뀔수 없다
+//let == 값이 바뀔수 있다.
+//var == 구시대의 유물 쓰지말것
+//boolean = true or false
+// null == 아무것도 없다.
+// undefined == 변수는 존재하지만 값이 정의되지 않음 ex) let var1;
+// null과 undefined 차이 == null은 자연적으로 발생하지 않음
+//2.5번부터 진행할것
+
+const amIFat = null;
+let somthing;
+console.log(somthing , amIFat);
