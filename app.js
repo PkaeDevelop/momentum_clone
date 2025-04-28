d2d23ec7efabdc090386b342a6f49622a65362f5
PkaeDevelop <rkdcksgh104@gmail.com>
2025-04-29 06:56:26 +0900

    52

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-function changeBackgroundColor() {
-  const widthPercent = (window.innerWidth / window.screen.width) * 100;
-  const body = document.body;
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-  // 기존 배경색 클래스 제거
-  body.classList.remove("skyblue-bg", "purple-bg", "yellow-bg");
-
-  if (widthPercent <= 50) {
-    body.classList.add("skyblue-bg");
-  } else if (widthPercent <= 90) {
-    body.classList.add("purple-bg");
-  } else {
-    body.classList.add("yellow-bg");
-  }
+function handleLoginBtnClick() {
+  const userName = loginInput.value;
+  console.log(userName);
 }
-
-// 창 크기 바뀔 때마다 실행
-window.addEventListener("resize", changeBackgroundColor);
-
-// 페이지 처음 열었을 때도 적용
-changeBackgroundColor();
+loginButton.addEventListener("click", handleLoginBtnClick);
