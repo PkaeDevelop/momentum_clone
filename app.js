d87560b4c371fc3c7dc7d9d0ae3b4cef40d37c7d
PkaeDevelop <rkdcksgh104@gmail.com>
2025-04-29 09:02:06 +0900

    4.3부터다시 수강
    4.3부터다시 수강

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function handleLoginBtnClick() {
+
+
+function onLoginSubmit(event) {
   const userName = loginInput.value;
+  event.preventDefault();
+  loginForm.classList.add("hidden");
   console.log(userName);
 }
+
+
+loginForm.addEventListener("submit",onLoginSubmit );
