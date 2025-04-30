928dc4e31b02be38f7b55c118cbc68348d715e21
PkaeDevelop <rkdcksgh104@gmail.com>
2025-04-30 20:43:00 +0900

    코드챌린지

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,23 @@
-const clock=document.querySelector("#clock");
+const clock = document.querySelector("#clock");
 
-function getClock(){
-    const date = new Date();
-    clock.innerText=`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-    //const hour = date.getHours();
+function getClock() {
+  const target = new Date("2025-12-25T00:00:00"); // 크리스마스 시간
+  const date = new Date(); // 현재시간
+
+  const CrsMas = target - date;
+  const seconds = Math.floor(CrsMas / 1000); // 초
+  const getMinutes = Math.floor(seconds / 60); // 분
+  const hours = Math.floor(getMinutes / 60); // 시간
+  const days = Math.floor(hours / 24); // 일
+
+  clock.innerText = `${pad(days)}d ${pad(hours % 24)}h ${pad(
+    getMinutes % 60
+  )}m ${pad(seconds % 60)}s`;
+}
+//두자리수 표현
+function pad(n) {
+  return String(n).padStart(2, "0");
 }
 
 getClock();
-setInterval(getClock,1000);
+setInterval(getClock, 1000);
